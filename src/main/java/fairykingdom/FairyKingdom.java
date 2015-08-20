33480278c87401931f239e1f374d51ed8156ff90
Lucas Olai Jarlkov Olsen <gamingtutsdk@gmail.com>
2015-08-20 21:44:11 +0200

    Added lang files
    Also added Cody to hardcoded author list

--- src/main/java/fairykingdom/FairyKingdom.java
+++ src/main/java/fairykingdom/FairyKingdom.java
@@ -29,7 +29,7 @@ public class FairyKingdom
     public static final String MODID = "fairykingdom";
     public static final String VERSION = "0.1";
     
-    public static final List<String> authorsCurrentVersion = Arrays.asList(new String[] {"Ruukas"});
+    public static final List<String> authorsCurrentVersion = Arrays.asList(new String[] {"Ruukas", "PirateCody"});
     public static ArrayList<String> authorsFromGithub;
 
 
